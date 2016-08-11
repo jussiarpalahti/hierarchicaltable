848919984e65f92122925939e90101cbd41b04c2
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-08-11 19:49:50 +0300

    Working compiler & reload toolchain
    Apparently WebStorm likes to think its TypeScript settings
    are better than project config's...

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+
+import {Table} from './table';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  React.createElement(Table, null),
+  document.getElementById('app'));
