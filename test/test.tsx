9b5b488180aa4eaabc4d3bacd4079de636b92e78
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-08-19 00:00:28 +0300

    Add ManualTable for comparative testing

--- test/test.tsx
+++ test/test.tsx
@@ -5,10 +5,18 @@ import { shallow } from 'enzyme';
 import { expect } from 'chai';
 
 import {Table} from '../src/table';
+import {ManualTable} from '../src/ManualTable';
 
 describe('<Table />', () => {
     it('renders a div o stuffing...', () => {
         const wrapper = shallow(<Table />);
         expect(wrapper.text()).to.equal('Test');
+    });
+});
+
+describe('<ManualTable />', () => {
+    it('renders a table of yore', () => {
+        const wrapper = shallow(<ManualTable />);
+        expect(wrapper.find('thead').children()).to.have.length(3);
     });
 });
