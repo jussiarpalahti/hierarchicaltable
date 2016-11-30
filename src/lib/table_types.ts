a433ead8062386f8b1f102271b9f489794dc8031
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-11-30 13:34:16 +0200

    Further clarification to Table model

--- src/lib/table_types.ts
+++ src/lib/table_types.ts
@@ -1,4 +1,12 @@
 
+/*
+
+Data model:
+    Table
+        -> Axis
+            -> Map Heading: [Header]
+
+ */
 
 class Header {
     name:string;
@@ -12,40 +20,53 @@ class Header {
     }
 }
 
-type Heading = Map<string, [Header]>;
+// TODO: What's the type for empty map?
+type THeading = Map<string, [Header]> | Map<{},{}>;
+
+class Axis {
+    headings:THeading;
+    size:number;
+    hops:number[];
+    loop:number[];
+    hop:Function[];
+
+    constructor(headings:THeading) {
+        this.headings = headings;
+    }
+}
 
 class Table {
     name: string;
     matrix:[string[]];
     title:string;
     url:string;
-    heading:Heading;
-    stub:Heading;
+    heading:Axis;
+    stub:Axis;
 
     constructor(name, matrix, title, url, heading?, stub?) {
         this.name = name;
         this.matrix = matrix;
         this.title = title;
         this.url = url;
-        this.heading = heading ? heading : new Map();
-        this.stub = stub ? stub : new Map();
+        this.heading = heading ? heading : new Axis(new Map());
+        this.stub = stub ? stub : new Axis(new Map());
     }
 
     add_heading(name:string, headers:[Header]) {
-        this.heading.set(name, headers);
+        this.heading.headings.set(name, headers);
     }
 
     add_stub(name:string, headers:[Header]) {
-        this.stub.set(name, headers);
+        this.stub.headings.set(name, headers);
     }
 
-    filter_headings(headings):Header[][] {
+    filter_headings(heading):Header[][] {
         /**
          * Creates table axis' headings with only visible headers
          */
 
         let resp = [];
-        headings.forEach(
+        heading.headings.forEach(
             (headers, name) => resp.push(headers.filter(header => header.show))
         );
         return resp;
@@ -58,6 +79,10 @@ class Table {
     show_stub() {
         return this.filter_headings(this.stub)
     };
+
+    size() {
+        return this.heading.size * this.stub.size;
+    }
 }
 
 export interface Dataset {
