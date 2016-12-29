849fb8f8469b23ac57a9f2020c59fcef9265fce3
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2016-12-29 18:29:51 +0200

    Add tests for selection behavior

--- test/test.tsx
+++ test/test.tsx
@@ -1,5 +1,3 @@
-// <reference path="../typings/index.d.ts" />
-
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
@@ -75,7 +73,6 @@ describe('Creating table', () => {
 
     it('!makes a table', () => {
        let new_table = new Table(small_dataset);
-       console.log("I'm tables", dir(new_table.view));
    });
 
     it('check hoppers', () => {
@@ -83,15 +80,26 @@ describe('Creating table', () => {
         let result = _.range(new_table.view.stub.size).map(() => {
             return new_table.view.stub.hop.map(hopper => hopper());
         });
-        expect(result[0][0].name).to.equal('x1');
-        expect(result[0][1].name).to.equal('y1');
+        expect(result[0][0].name).to.equal('a1');
+        expect(result[0][1].name).to.equal('b1');
         expect(result[1][0]).to.equal(null);
-        expect(result[1][1].name).to.equal('y2');
+        expect(result[1][2].name).to.equal('c2');
 
     });
 
     it('check selection', () => {
+        let new_table = new Table(small_dataset);
+        let first_headings_header = new_table.headings[0][0];
+        let third_stubs_header = new_table.stubs[0][2];
 
+        first_headings_header.deselect();
+        third_stubs_header.select();
+        new_table.update_view();
+
+        expect(new_table.view.heading.headers[0][0].name).to.equal('x2');
+        expect(new_table.view.heading.headers[1][0].name).to.equal('y1');
+        expect(new_table.view.stub.headers[0][0].name).to.equal('a1');
+        expect(new_table.view.stub.headers[0][2].name).to.equal('a3');
     });
 
     it('check table layout', () => {
