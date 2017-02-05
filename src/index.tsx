53bd404c968d7108271694b393032df04e590f81
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2017-02-05 23:19:40 +0200

    Matrix is now in the Table
    Take the red pill..

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,12 @@ import * as React from 'react';
 import {MainTable} from './table';
 import {Dataset, Table, get_table, get_preview_table, transform_table} from "./lib/table";
 
-export {Dataset, Table, Header, get_preview_table, get_table, transform_table} from './lib/table'
+export {Dataset, Table, Header, ITable, get_preview_table, get_table, transform_table} from './lib/table'
 
 export class HierarchicalTable extends React.Component<{table: Table}, {}> {
     render() {
         return (<div>
-            <MainTable table={this.props.table} matrix={this.props.table.matrix} />
+            <MainTable table={this.props.table} />
         </div>);
     }
 }
