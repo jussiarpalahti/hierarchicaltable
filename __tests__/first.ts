29caff8d2f1dd4ac5b06d584be1b420ce2657179
Jussi Arpalahti <jussi.arpalahti@hel.fi>
2017-06-25 22:34:18 +0300

    First new functional header hopper implementation!
    With tests!
    And full test coverage!

--- __tests__/first.ts
+++ __tests__/first.ts
@@ -1,5 +1,5 @@
 
-import {sum, minus} from '../src/code';
+import {sum, minus, looper} from '../src/code';
 
 it('adds 1 + 2 to equal 3 in Typescript', () => {
     expect(sum(1, 2)).toBe(3);
@@ -11,4 +11,13 @@ it('adds 1 + 2 to equal 3 in JavaScript', () => {
 
 it('makes sure coverage works', () => {
    expect(minus(2, 1)).toBe(1);
+});
+
+it('check continuous no-hop', () => {
+   expect(looper([1,2,3,4], 1, 0)).toBe(1);
+});
+
+it('check hopping headers', () => {
+    expect(looper([1,2,3,4], 4, 4)).toBe(2);
+    expect(looper([1,2,3,4], 4, 3)).toBe(false);
 });
--- src/code.ts
+++ src/code.ts
@@ -7,3 +7,12 @@ export function minus(a: number, b: number): number {
     return a - b;
 }
 
+export function looper(l:Array<String|Number>, repeat:number, n:number) {
+
+    if (repeat === 1) return l[n % l.length];
+    else if (n % repeat === 0) {
+        return l[(n / repeat) % repeat]
+    } else {
+        return false;
+    }
+}
